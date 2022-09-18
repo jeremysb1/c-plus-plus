010e40e6c93f675f685d7dd47b80b03727c3e61f
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-18 17:40:37 -0400

    added string datatype to exercise

--- hello.cpp
+++ hello.cpp
@@ -5,9 +5,11 @@ int main()
     int user_age = 34;
     double temperature = 20.6;
     bool is_alive = true;
+    std::string user_name = "Bob";
     
     std::cout << user_age << std::endl;
     std::cout << temperature << std::endl;
     std::cout << is_alive << std::endl;
+    std::cout << user_name << std::endl;
     return 0;
 }
