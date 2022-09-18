0d84aa8365ffb98d6ebd57f8f2d337f62e3e4db6
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-18 17:25:45 -0400

    added bool datatype to example

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,10 @@ int main()
 {
     int user_age = 34;
     double temperature = 20.6;
+    bool is_alive = true;
     
     std::cout << user_age << std::endl;
     std::cout << temperature << std::endl;
+    std::cout << is_alive << std::endl;
     return 0;
 }
