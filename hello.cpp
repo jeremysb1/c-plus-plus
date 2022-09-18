e86315ba8726b0c41e28053750486c95aa267826
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-18 17:23:11 -0400

    adding float data type example

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int a;
-
 int main()
 {
-    a = 2;
-    int b = a + 2;
-    std::cout << b <<std::endl;
+    int user_age = 34;
+    double temperature = 20.6;
+    
+    std::cout << user_age << std::endl;
+    std::cout << temperature << std::endl;
     return 0;
 }
