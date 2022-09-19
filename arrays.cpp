f256554cf6955ea61281ff4f6368625b9ae5cee2
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-19 19:11:25 -0400

    modified array element

--- arrays.cpp
+++ arrays.cpp
@@ -3,7 +3,9 @@
 int main()
 {
     double temp_list[4] = { 34.5, 27.8, 26.8, 22.0 };
+
+    temp_list[3] = 15.4;
     
-    std::cout << temp_list[1] << std::endl;
+    std::cout << temp_list[3] << std::endl;
     return 0;
 }
