ea866e7d0f448362587a3bfcdb82decd4b8339fe
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-06-25 11:30:28 -0500

    added temp conversion program

---
+++ temp_convert.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Fahrenheit = Celsius (°C) times 9/5 plus 32
+
+int main()
+{
+    double Fahrenheit{}, Celsius{};
+    std::cout << "Please enter the temperature in Celsius: ";
+    std::cin >> Celsius;
+
+    Fahrenheit = Celsius * (9.0 / 5.0) + 32;
+    std::cout << "The temperature in Fahrenheit is: " << Fahrenheit << std::endl;
+    return 0;
+}
