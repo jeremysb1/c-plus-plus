d0c8bdd85f0c19560a7d933beda1d7d602388c99
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-06-25 17:24:57 -0500

    updated settings and fixed error

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -2,15 +2,38 @@
 
 int main()
 {
+    int temperature;
+    int wind_speed;
     bool isWindy = false;
     bool isCold = true;
+    std::cout << "What is the temperature today in Celsius?";
+    std::cin >> temperature;
+    std::cout << "How strong is the wind outside from 1-10?";
+    std::cin >> wind_speed;
 
+    if (wind_speed > 5)
+    {
+        isWindy = true;
+    }
+    else
+    {
+        isWindy = false;
+    }
+    if (temperature <= 5)
+    {
+        isCold = true;
+    }
+    else 
+    {
+        isCold = false;
+    }
     if (isWindy && isCold)
     {
         std::cout << "I am not running" << std::endl;
     }
-    else
-    {
-        std::cout << "I am running" << std::endl;
+    else{
+        std::cout << "I am running." << std::endl;
+        
     }
-}
+}  
+
