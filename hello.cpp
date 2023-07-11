e8823beaf0f28ef7a70620194a693add4804a583
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-07-11 10:19:59 -0500

    added indexing

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,12 @@
 int main()
 {
     char word[] = { 'h', 'e', 'l', 'l', 'o', '\0' };
-    std::cout << word << std::endl;
+
+    char first_letter = word[0];
+    char last_letter = word[4];
+
+    std::cout << "The first letter of the word is: " << first_letter << std::endl;
+    std::cout << "The last letter of the word is: " << last_letter << std::endl;
+
+    return 0;
 }
