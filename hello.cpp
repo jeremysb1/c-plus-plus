4199d799bdef3b20ee036a804f0e7a13ae8f56c7
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-07-11 10:14:36 -0500

    added simple array program

--- hello.cpp
+++ hello.cpp
@@ -2,17 +2,6 @@
 
 int main()
 {
-    const int seconds_per_hour = 3600;
-    const double max_allowed_temperature = 76.8;
-    
-    int user_age = 34;
-    double temperature = 20.6;
-    bool is_alive = true;
-    std::string user_name = "Bob";
-    
-    std::cout << user_age << std::endl;
-    std::cout << temperature << std::endl;
-    std::cout << seconds_per_hour << std::endl;
-    std::cout << max_allowed_temperature << std::endl;
-    return 0;
+    char word[] = { 'h', 'e', 'l', 'l', 'o', '\0' };
+    std::cout << word << std::endl;
 }
